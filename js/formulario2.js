4e02012f26f9525e659fba8d7e5fd459ae1149c9
alejandroyndriago <alejandroyndriago@gmail.com>
2023-07-04 08:39:22 -0500

    se suben cambios al main para feed

--- js/formulario2.js
+++ js/formulario2.js
@@ -48,9 +48,10 @@ document.addEventListener("DOMContentLoaded", function() {
   
       Swal.fire({
         icon: 'success',
-        title: 'Registro exitoso',
+        title: 'Bienvenido a LOOK-UP',
+        text: 'Registro exitoso',
         showConfirmButton: false,
-        timer: 2000
+        timer: 4000
       }).then(() => {
         // Código adicional que puedes agregar después de un registro exitoso
         window.location.href='./pages/feed.html'
