c8492535388e1b5899450b77ef88a0fb20ef886e
alejandroyndriago <alejandroyndriago@gmail.com>
2023-07-05 09:00:24 -0500

    Funcion recuperar contraseña

--- js/formulario.js
+++ js/formulario.js
@@ -2,17 +2,24 @@ const login = document.querySelector('#login');
 const register = document.querySelector('#register');
 const registerLink = document.querySelector('#register-link');
 const loginLink=document.querySelector('#login-link');
-
-
-
+const forgetPassword = document.querySelector('#forgetPassword');
+const passwordLink = document.querySelector('#password-link');
+const loginLinkk=document.querySelector('#login-linkk');
 
 registerLink.addEventListener('click', () => mostrarocultarlogreg()) 
 
 
 loginLink.addEventListener('click', () => mostrarocultarlogreg()) 
 
+passwordLink.addEventListener('click', () => mostrarocultarlogregg()) 
+loginLinkk.addEventListener('click', () => mostrarocultarlogregg()) 
+
 const mostrarocultarlogreg=() => {
     login.classList.toggle('hidden')
     register.classList.toggle('hidden')
 }
 
+const mostrarocultarlogregg=() => {
+    login.classList.toggle('hidden')
+    forgetPassword.classList.toggle('hidden')
+}
